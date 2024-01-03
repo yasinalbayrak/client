10cdfa01ada5be3c9e7a13bfb684034f7fc90e22
Sıla Özinan <silaozinan@sabanciuniv.edu>
2024-01-03 20:59:20 +0300

    transcript pdf name made clicakble

--- src/components/transcriptPageComponents/transcriptUploadPage.jsx
+++ src/components/transcriptPageComponents/transcriptUploadPage.jsx
@@ -18,7 +18,6 @@ import { useSelector } from "react-redux";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import handleError, {handleInfo} from "../../errors/GlobalErrorHandler"
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
@@ -34,6 +33,7 @@ const TranscriptPage = (props) => {
   const { id } = useParams();
   const [transcript, setTranscript] = useState(null);
   const [consentChecked, setConsentChecked] = useState(false);
+  const [fileUrl, setFileUrl] = useState("");
   const [filename, setFilename] = useState(() => {
     const initialFileName = "No File Uploaded";
     return initialFileName;
@@ -80,6 +80,8 @@ const TranscriptPage = (props) => {
       setTranscript(file);
       const { name } = file;
       setFilename(name);
+      const fileUrl = URL.createObjectURL(file);
+      setFileUrl(fileUrl);
     };
     const onFileSubmit = () => {
       
@@ -93,6 +95,12 @@ const TranscriptPage = (props) => {
       }
     }, [transcript]);
 
+
+    const openFile = () => {
+      if (fileUrl) {
+        window.open(fileUrl, '_blank'); // Open the file in a new tab
+      }
+    };
     console.log(id);
   
     return (
@@ -123,7 +131,7 @@ const TranscriptPage = (props) => {
                   </Grid>
                   <Grid item xs={6}>
                     <Grid item container direction="rows">
-                      <Button variant="contained" component="label" onClick={onFileSubmit}  size="small"
+                      <Button variant="contained" component="label" onClick={onFileSubmit} size="small"
                               sx={{
                                 height: '45px',
                                 fontSize: '0.750rem',
@@ -133,12 +141,13 @@ const TranscriptPage = (props) => {
                       </Button>
                       <Typography alignItems="center" justifyContent="center" textAlign="center" m={2}>
                         {filename !== "No File Uploaded" ? (
-                            <>
-                              <FontAwesomeIcon icon={faFilePdf} style={{ color: 'red' }}/>
-                              <span style={{ fontSize: 'smaller', marginLeft: '0.3em' }}>{filename}</span>
-                            </>
+                            <button onClick={openFile}
+                                    style={{background: 'none', border: 'none', padding: 0, cursor: 'pointer'}}>
+                              <FontAwesomeIcon icon={faFilePdf} style={{color: 'red',fontSize: '18px'}}/>
+                              <span style={{fontSize: 'larger', marginLeft: '0.3em'}}>{filename}</span>
+                            </button>
                         ) : (
-                            <span style={{ fontSize: 'smaller' }}>{filename}</span>
+                            <span style={{fontSize: 'smaller'}}>{filename}</span>
                         )}
                       </Typography>
                     </Grid>
