3e8b1b3d4d39fcde1a847bda9adc557f28761aa0
Yasin Albayrak <yasin.albayrak@yildiztech.com.tr>
2024-02-08 11:50:22 +0300

    mini fix

--- src/pages/ApplyPage.js
+++ src/pages/ApplyPage.js
@@ -87,7 +87,7 @@ const ApplyPage = (props) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const currentTranscript = await getCurrentTranscript();
+        const currentTranscript = await getCurrentTranscript(userID);
         setstudentInfo(currentTranscript);
         for (const [c,g] of Object.entries(currentTranscript?.course)){
           if (g.courseCode == courseCode){
