9fe57d00e3c3c71789774d03e67f3bc2bf5dbd51
Yasin Albayrak <yasin.albayrak@yildiztech.com.tr>
2024-04-27 20:08:56 +0300

    error handler notifications are visible now

--- src/errors/GlobalErrorHandler.jsx
+++ src/errors/GlobalErrorHandler.jsx
@@ -2,22 +2,34 @@ import { toast } from 'react-toastify';
 
 export default function handleError(error) {
   if (error.response.status === 401) {
-    toast.error("Please login again.");
+    toast.error("Please login again.", {
+      containerId: "1618",
+      closeOnClick: true,
+    });
     window.location.replace("*");
   }
   if (error.response) {
     //console.log(error.response);
-    toast.error(`Error: ${error.response.status} - ${error.response.data.message ?? error.response.data.detail}`);
+    toast.error(`Error: ${error.response.status} - ${error.response.data.message ?? error.response.data.detail}`, {
+      containerId: "1618",
+      closeOnClick: true,
+    });
   } else if (error.message) {
-    toast.error(`Network Error: ${error.message}`);
+    toast.error(`Network Error: ${error.message}`,{
+      containerId: "1618",
+      closeOnClick: true,
+    });
   } else {
-    toast.error('An error occurred.');
+    toast.error('An error occurred.',{
+      containerId: "1618",
+      closeOnClick: true,
+    });
   }
 
   throw error
 };
 
-const CustomToastContent = ({ info, onMouseEnter, onMouseLeave}) => {
+const CustomToastContent = ({ info, onMouseEnter, onMouseLeave }) => {
   return (
     <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       {info}
@@ -41,12 +53,15 @@ export function handleInfo(info) {
     info={info}
     onMouseEnter={handleMouseEnter}
     onMouseLeave={handleMouseLeave}
-    
+
   />, {
     containerId: "1618",
     closeOnClick: true,
   });
 }
 export function handleServerDownError() {
-  toast.error("Server is not responding right now :(")
+  toast.error("Server is not responding right now :(",{
+    containerId: "1618",
+    closeOnClick: true,
+  })
 }
